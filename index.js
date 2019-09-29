0a675bbfa73f0b72e1739c5c7a66acc4600ca7b0
Fábio Santos <fabiosantosart@gmail.com>
2019-09-29 19:00:22 +0100

    fix #2 by handling overflow of the repeat variable

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 module.exports = function(length) {
-    var last = null
+    var period = Math.pow(10, 2)
+    , last = -1
     , repeat = 0
+    , overflow = 0
 
     if (typeof length == 'undefined') length = 15
 
     return function() {
-        var now = Math.pow(10, 2) * +new Date()
+        var now = period * +new Date()
 
-        if (now == last) {
+        if (now <= last) {
             repeat++
+            if (repeat === period) {
+                last += period
+                overflow += period
+                repeat = 0
+            }
         } else {
             repeat = 0
             last = now
+            overflow = 0
         }
 
-        var s = (now + repeat).toString()
+        var s = (now + overflow + repeat).toString()
         return +s.substr(s.length - length)
     }
 }
